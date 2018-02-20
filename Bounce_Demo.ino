3b7b7f1f01dadcb1f83be8ea5834cd345557823e
Michael Zhang <mzhang@bwc.com>
2018-02-20 10:40:23 -0800

    first commit

---
+++ Bounce_Demo.ino
@@ -0,0 +1,187 @@
+
+#include <SPI.h> 
+#include <Wire.h>
+#include <VL6180X.h>
+#include <SparkFun_ADXL345.h>
+#include <stdbool.h>
+
+extern "C" 
+{ 
+    #include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
+
+//Multiplexer Addresses
+#define TCAADDR1 0x70
+#define TCAADDR2 0x74
+
+//Pins to interface with Stepper I/O
+#define M_DIR 11
+#define M_EN A4
+#define M_VELO 10  //9is linked to LED
+#define LED_RED 9
+
+#define PWM_percent(val)  (255*val/100)
+//Accelerometer
+ADXL345 accelerometer = ADXL345();
+
+VL6180X laser_sensor[4]; //M_sensor_left;M_sensor_right;I_sensor_left;I_sensor_right;
+int channel[4] ={4,3,4,3};
+uint8_t multiplexer_ID[4]={TCAADDR1,TCAADDR1,TCAADDR2,TCAADDR2};
+
+
+//Multiplexer selection (multiplexer address, multiplexer channel)  
+void tcaselect(uint8_t multiplexer_address, uint8_t i) 
+{
+    if (i > 7) return;
+    Wire.beginTransmission(multiplexer_address);
+    Wire.write(1 << i);
+    Wire.endTransmission();  
+}
+
+void close_plexer(uint8_t multiplexer_ID)
+{
+    tcaselect(TCAADDR1,0);
+    tcaselect(TCAADDR2,0);    
+}
+
+
+/*
+Laser initialize loop.
+Check VL6180X documentation for register details.
+*/
+void Laser_initialize()
+{
+    for (int i=0;i<4;i++)
+    {
+        close_plexer(multiplexer_ID[i]);
+        tcaselect(multiplexer_ID[i],channel[i]);
+        laser_sensor[i].init();
+        laser_sensor[i].configureDefault();
+        laser_sensor[i].writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
+        laser_sensor[i].writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
+        laser_sensor[i].setTimeout(500);
+        laser_sensor[i].stopContinuous();
+        delay(10);    
+    }
+
+    delay(300);
+
+    for (int i=0;i<4;i++)
+    {
+        close_plexer(multiplexer_ID[i]);
+        tcaselect(multiplexer_ID[i],channel[i]);
+        laser_sensor[i].startInterleavedContinuous(100);
+        
+    }
+}
+
+
+int Laser_read(int i)
+{
+    close_plexer(multiplexer_ID[i]);
+    tcaselect(multiplexer_ID[i],channel[i]);
+    return laser_sensor[i].readRangeContinuousMillimeters();
+}
+
+void accelerometer_initialize()
+{
+
+  accelerometer.powerOn();                     // Power on the accelerometer345
+  accelerometer.setRangeSetting(2);           // Give the range settings "2" for +/-2G range
+
+}
+
+
+bool crash_detection(int acc_threshold)
+{
+    int x,y,z;
+
+    accelerometer.readAccel(&x, &y, &z);
+
+    if (x>=acc_threshold || y>=acc_threshold || z>=acc_threshold)
+    {
+        return true;
+    }
+
+    return false;
+
+}
+
+
+void crash_error()
+{
+    while (true)
+    {
+        //TODO blink RED LED
+        Pulse_led(LED_RED,50);
+    }
+
+}
+
+
+void Pulse_led(int LED_pin, int max_brightness)
+{
+for (int brightness = 0; brightness < max_brightness; brightness++) {
+      analogWrite(LED_pin, brightness);
+      delay(50);
+    }
+    delay(10);
+    // fade the LED on thisPin from brightest to off:
+    for (int brightness = max_brightness; brightness >= 0; brightness--) {
+      analogWrite(LED_pin, brightness);
+      delay(2);
+    }
+
+}
+
+void setup()
+{
+    Wire.begin();//Start wire so i2c transactions can take place
+    /*Pin direction on the BWCD
+    ---------------------outputs */
+    pinMode(M_DIR,OUTPUT);
+    pinMode(M_EN,OUTPUT);
+    pinMode(M_VELO,OUTPUT);
+
+    digitalWrite(M_EN,LOW); //Keep motor off during setup
+    analogWrite(M_VELO,PWM_percent(50)); //Set initial speed to 10%.  10% of whatever that is defined on the motor side
+    Serial.begin(115200);
+    Laser_initialize();
+    //Old_Laser_initialize();
+    accelerometer_initialize();
+    analogWrite(M_VELO,PWM_percent(100));
+    
+}
+     
+  
+void loop()
+{
+    
+    if (crash_detection(350))
+    {
+        digitalWrite(M_EN,LOW); //Motor is turned off
+        crash_error();
+        //TODO:  Add Pulse Red LED to next rev.
+    }
+
+    digitalWrite(M_EN,HIGH);
+    
+    for (int i=0;i<2;i++)
+    {        
+        if (Laser_read(i) < 50)
+        {
+            digitalWrite(M_DIR,LOW);
+        }
+       //Serial.println(Laser_read(i));     
+    }
+
+    for (int i=2;i<4;i++)
+    {        
+        if (Laser_read(i) < 50)
+        {
+            digitalWrite(M_DIR,HIGH);
+        }
+       //Serial.println(Laser_read(i));     
+    }
+
+}
